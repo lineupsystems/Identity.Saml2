f205edcd911440410c3aa185eceb8c42383a1271
Tomáš Bouda <email@tomasbouda.cz>
2025-03-19 15:07:45 +0100

    SamlHelper Decode/Encode functionality

--- SamlHelper/ViewModels/DecodeTabViewModel.cs
+++ SamlHelper/ViewModels/DecodeTabViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Linq;
 using System.Reactive;
+using System.Threading.Tasks;
 using System.Web;
 using System.Xml.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using ITfoxtec.Identity.Saml2.Http.Compression;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 using ReactiveUI;
 
 namespace SamlHelper.ViewModels;
@@ -16,27 +20,70 @@ public partial class DecodeTabViewModel : TabViewModel
     [ObservableProperty]
     public string _decodedSamlXml;
     
+    [ObservableProperty]
+    public bool _isQueryString;
+    
+    [ObservableProperty]
+    public string _signature;
+    
+    [ObservableProperty]
+    public string _signatureAlgorithm;
+    
     public ReactiveCommand<Unit, Unit> DecodeSamlCmd { get; }
 
     public DecodeTabViewModel()
     {
         Title = "Decode";
-        DecodeSamlCmd = ReactiveCommand.Create(DecodeSaml);
+        DecodeSamlCmd = ReactiveCommand.CreateFromTask(DecodeSaml);
     }
 
-    private void DecodeSaml()
+    private async Task DecodeSaml()
     {
         try
         {
-            var urlDecodedSaml = HttpUtility.UrlDecode(SamlToDecode);
-            var samlXml = SamlCompressionHelper.DecompressResponse(urlDecodedSaml);
+            string samlXml = "";
+            DecodedSamlXml = "";
+            
+            var queryParams = HttpUtility.ParseQueryString(SamlToDecode);
+            
+            if (queryParams.Count > 1)
+            {
+                IsQueryString = true;
+                    
+                var sAMLResponse = queryParams["SAMLResponse"];
+                Signature = queryParams["Signature"];
+                SignatureAlgorithm = queryParams["SigAlg"];
+                    
+                samlXml = SamlCompressionHelper.DecompressResponse(sAMLResponse);
+            }
+            else if (IsUrlEncoded(SamlToDecode))
+            {
+                var urlDecodedSaml = HttpUtility.UrlDecode(SamlToDecode);
+                samlXml = SamlCompressionHelper.DecompressResponse(urlDecodedSaml);
+            }
+            else
+            {
+                samlXml = SamlCompressionHelper.DecompressResponse(SamlToDecode);
+            }
+            
             XDocument doc = XDocument.Parse(samlXml);
             DecodedSamlXml = doc.ToString();
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex);
-            
+            var box = MessageBoxManager.GetMessageBoxStandard("Error", ex.Message);
+            await box.ShowAsync();
         }
+    }
+    
+    public static bool IsUrlEncoded(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return false;
+
+        string decoded = HttpUtility.UrlDecode(input);
+        string reEncoded = HttpUtility.UrlEncode(decoded);
+
+        return input.Equals(reEncoded, StringComparison.Ordinal);
     }
 }
--- SamlHelper/ViewModels/EncodeTabViewModel.cs
+++ SamlHelper/ViewModels/EncodeTabViewModel.cs
@@ -1,9 +1,46 @@
-﻿namespace SamlHelper.ViewModels;
+﻿using System;
+using System.Reactive;
+using System.Web;
+using System.Xml.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ITfoxtec.Identity.Saml2.Http.Compression;
+using ReactiveUI;
 
-public class EncodeTabViewModel : TabViewModel
+namespace SamlHelper.ViewModels;
+
+public partial class EncodeTabViewModel : TabViewModel
 {
+    [ObservableProperty]
+    public string _samlToEncode;
+
+    [ObservableProperty]
+    public string _compressedSamlXml;
+    
+    [ObservableProperty]
+    public string _urlEncodedSamlXml;
+    
+    public ReactiveCommand<Unit, Unit> EncodeSamlCmd { get; }
+
     public EncodeTabViewModel()
     {
         Title = "Encode";
+        EncodeSamlCmd = ReactiveCommand.Create(DecodeSaml);
+    }
+
+    private void DecodeSaml()
+    {
+        try
+        {
+            CompressedSamlXml = "";
+            UrlEncodedSamlXml = "";
+            
+            CompressedSamlXml = SamlCompressionHelper.CompressRequest(SamlToEncode);
+            UrlEncodedSamlXml = HttpUtility.UrlEncode(CompressedSamlXml);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            
+        }
     }
 }
